b8596c1053707d968474ae398c6c5611d26f07a4
Chakrit Thong Ek <49002416+thongekchakrit@users.noreply.github.com>
2022-08-15 01:51:27 +0800

    increased default size

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function  Home({ posts }) {
 
   return (
 
-    <div className="container mx-auto px-5 mb-8"> 
+    <div className="category-showcase container mx-auto px-5 mb-8"> 
       <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
 
       <Script strategy="lazyOnload">
