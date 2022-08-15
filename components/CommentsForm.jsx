afe7ad6d4c888681b9fdce09a9ebd1dce455d216
Chakrit Thong Ek <49002416+thongekchakrit@users.noreply.github.com>
2022-08-15 20:28:44 +0800

    revert commit

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -54,16 +54,16 @@ const CommentsForm = ({slug}) => {
 
   return (
     <div className='post-card-bg shadow-lg rounded-lg p-8 pb-12 mb-8'>
-        <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
+        <h3 className='text-xl mb-8 font-semibold border-b pb-6'>
         Leave a Comment
         </h3>
-        <div className='grid grid-cols-1 gap-4 mb-4'>
+        <div className='grid grid-cols-1 gap-6 mb-6'>
             <textarea ref={commentEl} className='p-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700'
             placeholder="Comment"
             name="comment"
             />
         </div>
-        <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4'>
+        <div className='grid grid-cols-1 lg:grid-cols-2 gap-6 mb-6'>
             <input type='text' ref={nameEl}
              className='py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700'
              placeholder="Name"
@@ -75,7 +75,7 @@ const CommentsForm = ({slug}) => {
                 name="email"
             />
         </div>
-        <div className='grid grid-cols-1 gap-4 mb-4'>
+        <div className='grid grid-cols-1 gap-6 mb-6'>
             <div>
               <input ref={storeDataEl} type="checkbox" id="storeData" name="storeData" value="true"/>
               <label className='text-gray-500 cursor-pointer ml-2'> Save my e-mail and name for next session</label>
