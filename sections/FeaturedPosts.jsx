a66754a6307b05d81e0e9839c8ede5e48922052a
Chakrit Thong Ek <49002416+thongekchakrit@users.noreply.github.com>
2022-08-15 21:33:55 +0800

    Revert "conflict"
    This reverts commit a3ebb0346a41f7d7c86d3a495bde68be0728128e.

--- sections/FeaturedPosts.jsx
+++ sections/FeaturedPosts.jsx
@@ -1,10 +1,6 @@
 import React, { useState, useEffect } from 'react';
 <<<<<<< HEAD
-<<<<<<< HEAD
 // import Carousel from 'react-multi-carousel';
-=======
-//import Carousel from 'react-multi-carousel';
->>>>>>> parent of d08ba39 (Revert "change font to poppin")
 =======
 //import Carousel from 'react-multi-carousel';
 >>>>>>> parent of d08ba39 (Revert "change font to poppin")
