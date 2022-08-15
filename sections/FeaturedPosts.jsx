d08ba39f27bae75440c21813adb9fd115b0f1a12
Chakrit Thong Ek <49002416+thongekchakrit@users.noreply.github.com>
2022-08-15 20:28:32 +0800

    Revert "change font to poppin"
    This reverts commit 254fbce9b669cb572a5e388e5ea5c6322561dd16.

--- sections/FeaturedPosts.jsx
+++ sections/FeaturedPosts.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-//import Carousel from 'react-multi-carousel';
-// import 'react-multi-carousel/lib/styles.css';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 import { FeaturedPostCard } from '../components';
 import { getFeaturedPosts } from '../services';
